2f5a002a98257026b5add9c78f5da8292c48111a
Ankita Singh <ankitasingh2a@gmail.com>
2018-11-09 22:20:45 +0530

    Added variables for slides

--- mod_dd_j_mod_slider/mod_dd_j_mod_slider.php
+++ mod_dd_j_mod_slider/mod_dd_j_mod_slider.php
@@ -12,6 +12,8 @@ require_once __DIR__ . '/helper.php';
 
 
 $class_sfx = htmlspecialchars($params->get('class_sfx'));
-$slides = $params->get('dd_article_slides_fields');
+$slides = &modDD_J_mod_sliderHelper::getSlides();
+$slideFields = $params->get('dd_article_slides_fields');
+$totalSlides = count((array)$slideFields);
 
 require(JModuleHelper::getLayoutPath('mod_dd_j_mod_slider', $params->get('layout', 'default')));
